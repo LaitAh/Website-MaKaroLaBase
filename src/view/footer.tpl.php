b101d083088e15ca9ba9e1b31b6c7606510e53e1
LaitAh <lea.clair34@gmail.com>
2022-08-12 19:49:15 +0200

    update(style): updating footer style for desktop media

--- src/view/footer.tpl.php
+++ src/view/footer.tpl.php
@@ -3,17 +3,18 @@
     <footer>
       <h4 class="footer-title">maKaro la Base</h4>
       <p class="footer-text">Cuisine vegan bio faites maison, 100% gourmande</p>
-      <ul class="footer-list" id="footer-left">
-        <li class="footer-element" id="footer-email"><a href="mailto:contact@makarolabase.com"><span class="footer-icon">&#9993; </span>contact@makarolabase.com</a></li>
-        <li class="footer-element" id="footer-phone"><span class="footer-icon">&#9742; </span>04.91.79.56.70</li>
-        <li class="footer-element" id="footer-address"><span class="footer-icon">&#9873; </span>2A boulevard des Cèdres, 13009 Marseille</li>
-      </ul>
-      <ul class="footer-list" id="footer-right">
-        <li class="footer-element"><a href="#">Plan du site</a></li>
-        <li class="footer-element"><a href="#">Mentions légales</a></li>
-        <li class="footer-element"><a href="#">CGV</a></li>
-      </ul>
+      <div class="footer-infos">
+        <ul class="footer-list" id="footer-left">
+          <li class="footer-element" id="footer-email"><a href="mailto:contact@makarolabase.com"><span class="footer-icon">&#9993; </span>contact@makarolabase.com</a></li>
+          <li class="footer-element" id="footer-phone"><span class="footer-icon">&#9742; </span>04.91.79.56.70</li>
+          <li class="footer-element" id="footer-address"><span class="footer-icon">&#9873; </span>2A boulevard des Cèdres, 13009 Marseille</li>
+        </ul>
+        <ul class="footer-list" id="footer-right">
+          <li class="footer-element"><a href="#">Plan du site</a></li>
+          <li class="footer-element"><a href="#">Mentions légales</a></li>
+          <li class="footer-element"><a href="#">CGV</a></li>
+        </ul>
+      </div>
     </footer>
-    
   </body>
 </html>
