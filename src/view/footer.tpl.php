8af6ddf0680c3ccad549d3d9b12b91f66970c6d2
LaitAh <lea.clair34@gmail.com>
2022-08-13 12:44:59 +0200

    update(footer): updating icon of email, phone and address

--- src/view/footer.tpl.php
+++ src/view/footer.tpl.php
@@ -5,9 +5,9 @@
       <p class="footer-text">Cuisine vegan bio faites maison, 100% gourmande</p>
       <div class="footer-infos">
         <ul class="footer-list" id="footer-left">
-          <li class="footer-element" id="footer-email"><a href="mailto:contact@makarolabase.com"><span class="footer-icon">&#9993; </span>contact@makarolabase.com</a></li>
-          <li class="footer-element" id="footer-phone"><span class="footer-icon">&#9742; </span><a href="tel:+33491795670"></a> 04.91.79.56.70</li>
-          <li class="footer-element" id="footer-address"><span class="footer-icon">&#9873; </span>2A boulevard des Cèdres, 13009 Marseille</li>
+          <li class="footer-element" id="footer-email"><a href="mailto:contact@makarolabase.com"><sub><img src="<?= $absoluteUrl ?>/img/icon/icon-mail_white.svg" alt="Icône d'email" class="footer-icon"></sub> contact@makarolabase.com</a></li>
+          <li class="footer-element" id="footer-phone"><a href="tel:+33491795670"><sub><img src="<?= $absoluteUrl ?>/img/icon/icon-phone_white.svg" alt="Icône de téléphone" class="footer-icon"></sub> 04.91.79.56.70</a></li>
+          <li class="footer-element" id="footer-address"><sub><img src="<?= $absoluteUrl ?>/img/icon/icon-hous_white.png" alt="Icône de maison" class="footer-icon"></sub> 2A boulevard des Cèdres, 13009 Marseille</li>
         </ul>
         <ul class="footer-list" id="footer-right">
           <li class="footer-element"><a href="#">Plan du site</a></li>
