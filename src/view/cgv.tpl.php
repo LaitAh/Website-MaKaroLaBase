7a1eb7727e87553522ad638ab6958d3eebc14307
LaitAh <lea.clair34@gmail.com>
2023-03-05 17:57:31 +0100

    new page for cgv and mentions légales

---
+++ src/view/cgv.tpl.php
@@ -0,0 +1,111 @@
+<section class="cgv">
+  <div class="cgv-top">
+    <!-- <img src="<?= $absoluteUrl ?>/img/nav/icon-cgv.jpg" alt="Image de cgv" class="cgv-icon"> -->
+    <h1 class="cgv-title">mentions légales & cgv</h1>
+  </div>
+  <div class="mentionslegales">
+    <h2>mentions légales</h2>
+    <div class="mentionslegales-maKaro">
+      <h3>identification du professionnel</h3>
+      <p>
+        maKaro la Base<br/>
+        entrepreneur individuel<br/>
+        Karolina Jasniak<br/>
+        2a boulevard des cèdres<br/>
+        13009 Marseille
+      </p>
+      <p>
+        Siret : 884.399.908<br/>
+        RCS/RM : 05
+      </p>
+      <p>
+        <a href="mailto:contact@makarolabase.com">contact@makarolabase.com</a><br/>
+        <a href="tel:+33491795670">04.91.79.56.70</a>
+      </p>
+    </div>
+    <div class="mentionslegales-hebergeur">
+      <h3>identification de l'hébergeur</h3>
+      <p>
+        o2switch<br/>
+        Siret : 510 909 807 00032<br/>
+        RCS Clermont Ferrand<br/>
+        SAS au capital de 100 000€
+      </p>
+      <p>
+        Opérateur Télécom déclaré ARCEP<br/>
+        09/2989 – AS50474
+      </p>
+      <p>Marque déposée INPI : 09 3 645 279</p>
+    </div>
+  </div>
+  <div class="cgvdetails">
+    <h2>conditions générales de vente</h2>
+    <p>Le présent document (Condition Générales de Vente) définit les conditions applicables aux ventes conclues entre d'une part les personnes effectuant une commande via le site Internet de maKaro la Base <a href="https://www.makarolabase.com" class="link">https://www.makarolabase.com</a>, ci-après dénommées "le client" et d'autre part micro-entreprise maKaro la Base d'Entrepreneur Individuel Karolina Jasniak dont l'adresse est au 2a boulevard des cèdres - 13009 Marseille.</p>
+    <div class="services">
+      <h3>CARACTÉRISTIQUES ESSENTIELLES DES SERVICES</h3>
+      <p>L'entreprise de Madame Jasniak propose des produits végétaliens sucrés et salés.<br/>
+      Disponibles dans les points de vente ou sous livraison.</p>
+    </div>
+    <div class="prix">
+      <h3>PRIX</h3>
+      <p>Le prix des produits proposés sur <a href="https://www.makarolabase.com" class="link">www.makarolabase.com</a> sont indiqués en euro toutes taxes comprises. La livraison est offerte.
+      Madame Jasniak se réserve le droit de modifier ses prix à tout moment et sans préavis.</p>
+    </div>
+    <div class="commande">
+      <h3>COMMANDE</h3>
+      <p>Le client peut commander via e-mail : <a href="mailto:contact@makarolabase.com" class="link">contact@makarolabase.com</a> 24h/24 7j/7 ou par téléphone au <a href="tel:+33491795670">04.91.79.56.70</a> (prix d'un appel local) 7j/7 de 8h à 22h, ou encore via plate-messagerie Instagram. La commande doit être passée la veille au plus tard.<br/>
+      Les commandes dépassants 100€ nécessitent un acompte de 30%.</p>
+      <p>Madame Jasniak se réserve le droit d'annuler ou de refuser toute commande d'un client qui présente à ses yeux une quelconque forme de risque.</p>
+    </div>
+    <div class="livraison">
+      <h3>LIVRAISON</h3>
+      <p>La commande peut être livrée uniquement dans la zone de livraison, soit du 1<sup>er</sup> au 12<sup>e</sup> arrondissement.</p>
+    </div>
+    <div class="paiement">
+      <h3>PAIEMENT</h3>
+      <p>Le paiement des commandes s'effectue au choix par :</p>
+      <ul>
+        <li>espèces</li>
+        <li>cheques</li>
+        <li>virement (uniquement les clients professionnels)</li>
+      </ul>
+      <p>
+        Conditions de paiement :
+      </p>
+      <ul>
+        <li>clients particuliers : à la réception de la marchandise</li>
+        <li>clients professionnels : sous 15 jours suivant la réception de la marchandise</li>
+      </ul>
+    </div>
+    <div class="retour">
+      <h3>ÉCHANGE, RETOUR ET REMBOURSEMENT</h3>
+      <p>Les produits ne sont ni échangeables, ni remboursables et les retours ne sont pas acceptés.</p>
+    </div>
+    <div class="annulation">
+      <h3>ANNULATION</h3>
+      <p>Le commande en cour doit être annulé 24h avant la livraison. Les commandes à plus de 100&#8364; doivent être annulé 72h avant la livraison, dépassant ce délai l'acompte de 30% ne sera pas remboursé.</p>
+    </div>
+    <div class="reclamation">
+      <h3>RÉCLAMATION</h3>
+      <p>Si besoin est, maKaro la Base est joignable par :</p>
+      <ul>
+        <li>e-mail : <a href="mailto:contact@makarolabase.com">contact@makarolabase.com</a> (réponse sous 24H)</li>
+        <li>téléphone au <a href="tel:+33491795670">04.91.79.56.70</a></li>
+      </ul>
+      <p>En cas de litige, une solution amiable sera recherchée. N'hésitez pas à nous contacter.</p>
+    </div>
+    <div class="garanties">
+      <h3>GARANTIES</h3>
+      <p>Le produit doit être consommé dans les 4 jours et doit être gardé au frais.</p>
+    </div>
+    <div class="responsabilites">
+      <h3>RESPONSABILITÉS</h3>
+      <p>Les photographies des produits représentées sur le Site <a href="https://www.makarolabase.com" class="link">www.makarolabase.com</a> sont les plus fidèles possibles mais ne peuvent garantir une similitude parfaite avec les produits proposés, notamment en ce qui concerne les couleurs.</p>
+    </div>
+    <div class="donnees">
+      <h3>PROTECTIONS DES DONNÉES PERSONNELLES</h3>
+      <p>Les données collectées sont destinées uniquement à l'usage de maKaro la Base. Elles sont nécessaires au traitement et à la gestion des commandes du client.</p>
+      <p> Vos droits. Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez d'un droit d'accès et de rectification des données vous concernant.</p>
+    </div>
+  </div>
+</section>
